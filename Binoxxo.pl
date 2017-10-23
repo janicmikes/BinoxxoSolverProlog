f6c050dc14b38ae33bb746095f1e4760906a3b9a
Kasacka <joel.egger@hsr.ch>
2017-10-23 14:19:52 +0200

    Update Binoxxo.pl

--- Binoxxo.pl
+++ Binoxxo.pl
@@ -1,3 +1,46 @@
+% Anzahl Vorkommen in einer Liste prüfen
+occurrences(_, [], 0).
+
+occurrences(Item, [Item | Tail], Nof) :-
+occurrences(Item, Tail, NofSub), Nof is NofSub + 1.
+
+occurrences(Item, [Head | Tail], Nof) :-
+occurrences(Item, Tail, Nof), Item \= Head.
+
+% Gleichheit der Häufigkeit von x und o prüfen
+symbol_equality(List) :-
+occurrences(o, List, Nof), occurrences(x, List, Nof).
+
+symbol_equality(List) :-
+occurrences(x, List, Nof), occurrences(o, List, Nof).
+
+% Chain prüft ob es eine Kette von mindestens Counter Elementen
+% gleichartiger Elemente hat
+valid_triple(x, x, o).
+valid_triple(x, o, x).
+valid_triple(o, o, x).
+valid_triple(o, x, o).
+valid_triple(x, o, o).
+valid_triple(o, x, x).
+
+neighbour_check([A, B, C]) :- valid_triple(A, B, C).
+
+neighbour_check([Head1, Head2, Head3 | Tail]) :-
+valid_triple(Head1, Head2, Head3),
+neighbour_check([Head2, Head3 | Tail]).
+
+% Binoxxo
+binoxxo_rows([]).
+
+binoxxo_rows([Row | OtherRows]) :-
+symbol_equality(Row),
+neighbour_check(Row),
+binoxxo_rows(OtherRows).
+
+binoxxo(Rows, Columns) :-
+binoxxo_rows(Rows),
+binoxxo_rows(Columns).
+
 :- use_module(library(clpfd)).
 
 binoxxoInput(x).
