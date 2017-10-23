2f60424ac3e9533859bd724021cd09943eba42ad
Janic Mikes <janic.mikes@gmail.com>
2017-10-23 16:31:59 +0200

    update binoxxo solver

--- Binoxxo.pl
+++ Binoxxo.pl
@@ -1,4 +1,16 @@
-use_module(library(clpfd)).
+% Matrix transpose function
+transpose([], []).
+transpose([F|Fs], Ts) :-
+    transpose(F, [F|Fs], Ts).
+
+transpose([], _, []).
+transpose([_|Rs], Ms, [Ts|Tss]) :-
+        lists_firsts_rests(Ms, Ts, Ms1),
+        transpose(Rs, Ms1, Tss).
+
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+        lists_firsts_rests(Rest, Fs, Oss).
 
 % Anzahl Vorkommen in einer Liste prüfen
 occurrences(_, [], 0).
@@ -42,28 +54,6 @@ binoxxo_rows(OtherRows).
 binoxxo(Rows) :-
 binoxxo_rows(Rows),
 transpose(Rows, Columns),
-binoxxo_rows(Columns).
+binoxxo_rows(Columns),
+maplist(writeln, Rows).
 
-binoxxo_problem(1,
-    [[A11, A21, A31, A41, x, A61, A71, o],
-	[o, A22, A32, A42, A52, A62, A72, A82],
-	[A13, x, o, A43, A53, A63, o, A83],
-	[A14, A24, A34, x, x, A64, A74, A84],
-	[x, A25, A35, o, A55, o, A75, A85],
-	[A16, A26, A36, x, A56, A66, A76, o],
-	[A17, A27, A37, A47, A57, A67, x, A87],
-	[o, x, o, A48, A58, o, A78, A88]]
-).
-
-transpose([], []).
-transpose([F|Fs], Ts) :-
-    transpose(F, [F|Fs], Ts).
-
-transpose([], _, []).
-transpose([_|Rs], Ms, [Ts|Tss]) :-
-        lists_firsts_rests(Ms, Ts, Ms1),
-        transpose(Rs, Ms1, Tss).
-
-lists_firsts_rests([], [], []).
-lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
-        lists_firsts_rests(Rest, Fs, Oss).
