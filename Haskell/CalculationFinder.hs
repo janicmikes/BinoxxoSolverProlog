1132d97a438270175c8792f26df574c97da64253
Rico Akermann <rakerman@hsr.ch>
2017-12-11 10:57:31 +0100

    print all possible solutions

--- Haskell/CalculationFinder.hs
+++ Haskell/CalculationFinder.hs
@@ -1,34 +1,32 @@
 import System.Environment
 import System.Exit
 
-powerset [] = [[]]
-powerset (head:tail) = subset ++ map (head:) subset
-                 where subset = powerset tail
+powset [] = [[]]
+powset (head:tail) = subset ++ [head:s | s <- subset]
+                 where subset = powset tail
 
 stringToInteger string = read string :: Integer
 
 calculateSum [] _ = 0
 calculateSum list [] = stringToInteger list
-
 calculateSum list (headSplit:tailSplits) = 
     (stringToInteger (take headSplit list)) +
     (calculateSum (drop headSplit list) (map (+(-headSplit)) tailSplits))
 
 checkCalculationSum list sum split = (calculateSum list split) == stringToInteger sum
 
-findCalculationPowerset _ _ [] = [-1]
+findCalculationPowset _ _ [] = []
+findCalculationPowset list sum splits = [x | x <- splits, checkCalculationSum list sum x]
 
-findCalculationPowerset list sum (headSplit:tailSplits) =
-    if (checkCalculationSum list sum headSplit) then headSplit
-    else (findCalculationPowerset list sum tailSplits)
-
-findCalculation list sum = findCalculationPowerset list sum (powerset [1..(length list - 1)])
+findCalculation list sum = findCalculationPowset list sum (powset [1..(length list - 1)])
 
 showCalculationResult [] calculation = calculation
 showCalculationResult (headSplit:tailSplits) calculation =
     take headSplit calculation ++ "+" ++ (showCalculationResult (map (+(-headSplit)) tailSplits) (drop headSplit calculation))
+    
+showCalculation splits sum = [s | x <- splits, s <- showCalculationResult x sum ++ "\n"]
 
 main = do
     args <- getArgs
-    print $ showCalculationResult (findCalculation (head args) (last args))  (head args)
+    putStrLn $ showCalculation (findCalculation (head args) (last args))  (head args)
     exitWith ExitSuccess
